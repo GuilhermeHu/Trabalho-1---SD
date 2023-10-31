6631c524d4e1458447f3307ed8bea2fa4beec78b
GuilhermeHu <149438896+GuilhermeHu@users.noreply.github.com>
2023-10-31 17:27:02 -0300

    Update somador.vhd

--- somador.vhd
+++ somador.vhd
@@ -29,20 +29,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity somador is
-    Port ( a : in  STD_LOGIC;
-           b : in  STD_LOGIC;
-           c_in : in  STD_LOGIC;
-           s : out  STD_LOGIC;
-           c_out : out  STD_LOGIC);
+entity somador is                         --somador: retorna a soma e carry do bit A com o bit B e com o carry_in (c_in, carry do bit anterior), std_logic -> std_logic
+    Port ( a : in  STD_LOGIC;             --entrada do bit A
+           b : in  STD_LOGIC;             --entrada do bit B
+           c_in : in  STD_LOGIC;          --entrada do bit c_in
+           s : out  STD_LOGIC;            --resultado da soma
+           c_out : out  STD_LOGIC);       --c_out da soma
 end somador;
-
+    
 architecture Behavioral of somador is
 
 begin
 
-s <= (a XOR b) XOR c_in;
-c_out <= (a AND b) OR (c_in AND (a OR b));
+s <= (a XOR b) XOR c_in;                         --soma de dois bits se dá pela expressão booleana [(A XOR B) XOR cin], encontrado através da simplificação da tabela verdade 
+c_out <= (a AND b) OR (c_in AND (a OR b));       --c_out de dois bits se dá pela expressão booleana [(A AND B) OR  (cin AND (A OR B))], encontrado através da tabela verdade 
 
 end Behavioral;
 

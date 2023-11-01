0e53f765d1dcc933418509d3f92487b9395c2af4
GuilhermeHu <149438896+GuilhermeHu@users.noreply.github.com>
2023-11-01 00:24:30 -0300

    Update par_ou_impar.vhd

--- par_ou_impar.vhd
+++ par_ou_impar.vhd
@@ -30,30 +30,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 
--- Esse módulo nos informa se o resultado da soma de dois números deu um número par ou
--- ímpar, semelhante ao jogo par ou ímpar que é cotidianamente realizado entre pessoas 
--- que querem definir algo. Definimos par como 0 e ímpar como 1 por causa de suas paridades.
+-- Esse módulo nos informa se o resultado da soma de dois números fornecidos deu um número par ou ímpar, semelhante ao jogo par ou ímpar 
+-- que é cotidianamente realizado entre as pessoas. Definimos par como 0 e ímpar como 1 por causa de suas paridades.
 entity par_ou_impar is
-    Port ( a : in  STD_LOGIC_VECTOR (3 downto 0);
-			  b : in  STD_LOGIC_VECTOR (3 downto 0);
-           s : out  STD_LOGIC;
-			  overflow : out STD_LOGIC;
-			  carry_out : out STD_LOGIC;
-			  bit_sinal : out STD_LOGIC;
-			  zero : out STD_LOGIC);
+    Port ( a : in  STD_LOGIC_VECTOR (3 downto 0);             --Entrada A, um número binário de 4 bits
+	   b : in  STD_LOGIC_VECTOR (3 downto 0);             --Entrada B, um número binário de 4 bits
+           s : out  STD_LOGIC;                                --Resultado da operação, sendo '0' quando o resultado da soma de A com B for um valor par, e sendo '1' quando o resultado for ímpar
+	   overflow : out STD_LOGIC;                          --Flag de overflow, apontando se houve overflow da soma entre A e B
+	   carry_out : out STD_LOGIC;                         --Flag de carry out, apontando se houve carry out da soma entre A e B
+	   bit_sinal : out STD_LOGIC;                         --Flag de sinal, que aponta se o resultado da soma entre A e B deu um valor positivo (0) ou negativo (1)
+	   zero : out STD_LOGIC);                             --Flag de zero, apontando se o resultado da soma entre A e B deu exatamente o valor zero ("0000")
 end par_ou_impar;
 
 architecture Behavioral of par_ou_impar is
 
-component somador_4bits is
-    Port ( a : in  STD_LOGIC_VECTOR (3 downto 0);
+component somador_4bits is                                    --Como esse módulo precisa somar A e B antes de verificar se o resultado é par ou ímpar,
+    Port ( a : in  STD_LOGIC_VECTOR (3 downto 0);             --chama-se o componente do somador de 4 bits para a execução da soma
            b : in  STD_LOGIC_VECTOR (3 downto 0);
            c_in : in  STD_LOGIC;
            s : out  STD_LOGIC_VECTOR (3 downto 0);
-			  overflow : out  STD_LOGIC;
+	   overflow : out  STD_LOGIC;
            carry_out : out  STD_LOGIC;
-			  bit_sinal : out STD_LOGIC;
-			  zero : out STD_LOGIC);
+	   bit_sinal : out STD_LOGIC;
+	   zero : out STD_LOGIC);
 end component;
 
 signal r : STD_LOGIC_VECTOR (3 downto 0);
@@ -65,11 +64,12 @@ signal zeron : STD_LOGIC;
 
 begin
 
-cin <= '0';
+cin <= '0';                  --C_in de uma soma sempre é 0
 
-soma: somador_4bits PORT MAP(a, b, cin, r, ov, c_out, bit_sin, zeron);
+soma: somador_4bits PORT MAP(a, b, cin, r, ov, c_out, bit_sin, zeron);               --Execução do somador de 4 bits
 
-s <= r(0);
+s <= r(0);                   --A paridade de um número depende exclusivamente de seu bit menos significativo, que representa, em decimal, o 2^0 (1). Assim,
+                             --um número binário que apresente como bit menos significativo o '1', é ímpar por em sua conversão para decimal somar-se 1
 overflow <= ov;
 carry_out <= c_out;
 bit_sinal <= '0';
